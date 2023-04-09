b814066e8a4978f138b09719e642b6ffd3ab2c7c
jimbo23 <kiefersoon@gmail.com>
2023-04-09 19:53:38 +0800

    Initial setup 😎

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
@@ -13,6 +9,19 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontSize: {
+      '2xsm': '10px',
+      xsm: '12px',
+      sm: '13px',
+      reg: '15px',
+      lg: '18px',
+      '2xl': '22px',
+      '3xl': '25px',
+      '4xl': '32px',
+      '5xl': '40px',
+      '6xl': '50px',
+      '7xl': '70px',
+    },
   },
   plugins: [],
-}
+};
