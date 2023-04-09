4e5bfe500663550661ba68671bd496689b461ecb
jimbo23 <kiefersoon@gmail.com>
2023-04-09 23:24:06 +0800

    Cleaning up with layout.tsx 🧹

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 import Header from '@/app/components/Header';
-import NavBar from '@/app/components/NavBar';
 import RestaurantCard from '@/app/components/RestaurantCard';
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <main>
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <RestaurantCard />
-          </div>
-        </main>
-      </main>
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        <RestaurantCard />
+      </div>
     </main>
   );
 }
