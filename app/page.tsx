822b88a26e2f7d2493087aa760b41e276cd16e16
jimbo23 <kiefersoon@gmail.com>
2023-04-24 22:29:24 +0800

    Making asynchronous call with server component ✌🏼

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,41 @@
 import Header from '@/app/components/Header';
 import RestaurantCard from '@/app/components/RestaurantCard';
+import { prisma } from '@/app/prisma';
+import { Cuisine, Location, PRICE } from '@prisma/client';
 
-export default function Home() {
+export type RestaurantCardType = {
+  id: number;
+  name: string;
+  main_img: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+};
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> =>
+  await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_img: true,
+      cuisine: true,
+      location: true,
+      price: true,
+      slug: true,
+    },
+  });
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+        ))}
       </div>
     </main>
   );
--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,6 +1,7 @@
 import MenuCard from '@/app/restaurant/[slug]/components/MenuCard';
+import { Item } from '@prisma/client';
 
-export default function Menu() {
+export default function Menu({ menu }: { menu: Item[] }) {
   return (
     <main className="bg-white mt-5">
       <div>
@@ -8,7 +9,13 @@ export default function Menu() {
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
         <div className="flex flex-wrap justify-between">
-          <MenuCard />
+          {menu.length > 0 ? (
+            menu.map((item) => <MenuCard key={item.id} item={item} />)
+          ) : (
+            <div className="flex flex-wrap justify-between">
+              <p>This restaurant does not have a menu.</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
